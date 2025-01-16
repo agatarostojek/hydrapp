52e65a8783b6f941140cbf572769fe85a48d3e1a
agatarostojek <a.rostojek@gmail.com>
2025-01-16 14:27:16 +0100

    adding localStorage function

--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ const counterValue = document.querySelector(".counter--js");
 
 let glasses = 0;
 
+/* Local storage key-date element */
+const key = new Date().toISOString().slice(0,10);
+
+if (localStorage.getItem(key))
+glasses = parseInt(localStorage.getItem(key));
+
+/* Glasses counter; buttons */
 const setCounterValue = (value) => {
     counterValue.innerHTML = value;
+    localStorage.setItem(key, value);
 }
 
 setCounterValue(glasses);
